e67e8471fa6f8ecd636af3ea1bd8f8beca858091
Victor Okeke <okekevicktur@gmail.com>
2023-02-21 00:26:47 +0100

    search icon cursor fixed

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
-const apiUrl= 'https://api.dictionaryapi.dev/api/v2/entries/en';
+// const apiUrl= 'https://api.dictionaryapi.dev/api/v2/entries/en';
 
-const searchWord = document.getElementsByClassName('search-icon');
+const searchWord = document.getElementById('searchwrap');
 const searchTextbox = document.getElementById('search');
 // const adviceTitle =  document.getElementById('advice-title');
 
-searchWord.addEventListener('click', function(){
-//    alert( randomizeSlip());
 
+searchWord.addEventListener('click', function(){
 
 //    adviceTitle.firstChild.innerText = `ADVICE #${randomizeSlip()}`;
     // displayAdvice();
     // document.getElementById("myText").value
-    let newWord=  searchTextbox.value;
-    alert(newWord);
+    // let newWord=  searchTextbox.value;
+    // alert(newWord);
+    // console.log(newWord);
     // getapi(apiUrl);
 })
 
 function displayAdvice(advice){
   
 
-        adviceTitle.firstChild.innerText = `ADVICE #${advice.id}`;
-        adviceText.firstChild.innerText = advice.advice;
+        // adviceTitle.firstChild.innerText = `ADVICE #${advice.id}`;
+        // adviceText.firstChild.innerText = advice.advice;
     
 }
 //Defining async function
-async function getapi(url,word){
-          //storing response
-        const response = await fetch(`${url}/${word}`);
+// async function getapi(url,word){
+//           //storing response
+//         const response = await fetch(`${url}/${word}`);
 
-        //storing data in form of JSon
-        var data = await response.json();
-        console.log(data);
-        return;
-        if (response) {
-            displayAdvice(data.slip)
-        }
+//         //storing data in form of JSon
+//         var data = await response.json();
+//         console.log(data);
+//         return;
+//         if (response) {
+//             displayAdvice(data.slip)
+//         }
        
     
-    }
+    // }
