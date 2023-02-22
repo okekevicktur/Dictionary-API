d02ca0e3fd02722e877c7880899b373fa5677162
Victor Okeke <okekevicktur@gmail.com>
2023-02-22 19:06:28 +0100

    display error fixed

--- app.js
+++ app.js
@@ -8,62 +8,104 @@ const partOfSpeech = document.getElementById('speech-type');
 const definition1 = document.getElementById('meaning1');
 const definition2 = document.getElementById('meaning2');
 const definition3 = document.getElementById('meaning3');
-const synonyms = document.getElementById('synonym');
+const synonyms = document.getElementById('synonymsis');
 const partOfSpeech2 = document.getElementById('speech-verb');
 const verbMeaning1 = document.getElementById('verbMeaning');
 const example = document.getElementById('example');
 const source = document.getElementById('sourceLink');
-
+const play = document.getElementById('play-wrap');
+const SecondMeaning = document.querySelector('.meaning-header');
 // const adviceTitle =  document.getElementById('advice-title');
 
 
+
+// play.addEventListener('click', function(){
+
+
+// })
 searchWord.addEventListener('click', function(){
-
-
+   
     let newWord=  searchTextbox.value;
     getapi(apiUrl,newWord);
 })
 function checkUndefined(isUndefined){
-    if ( isUndefined === undefined){
-        return "----";
-    }
+    if (isUndefined && isUndefined.length > 0 && isUndefined !== "undefined")  {
+        
+       return `${isUndefined}`;
+   }
+   else{
+         return   '';
+   }
 }
 
 function displayWord(wordArray){
-  
-    // console.log(element.phonetic);
+   
     word.innerText = `${wordArray.word}`;
-    phonetic.innerText = `${wordArray.phonetic}`;
-    partOfSpeech.innerText = `${wordArray.meanings[0].partOfSpeech}`;
-    definition1.innerText= `${wordArray.meanings[0].definitions[0].definition}`;
-    definition2.innerText= `${wordArray.meanings[0].definitions[1].definition}`;
-    definition3.innerText= `${wordArray.meanings[0].definitions[2].definition}`;
-    const synonymsArray= wordArray.meanings[1].synonyms;
-    if (synonymsArray.length != 0)  {
-        if (synonymsArray.length > 4){
-            // console.log(length.synonymsArray);const slicedArray = array.slice(0, n);
-            let  newSynonyms = synonymsArray.slice(0, 3);
-            synonyms.innerText= `${newSynonyms}`;
-           // return;
-        }
-        else { synonyms.innerText= `${synonymsArray}`;}
+    if(checkUndefined(`${wordArray.phonetic}`) === ''){
+         phonetic.innerText = checkUndefined(`${wordArray.phonetics[1].text}`);
     }else{
-        synonyms.innerText= ``;
+        phonetic.innerText = checkUndefined(`${wordArray.phonetic}`);
     }
-    
-    //Display Verb section
-    partOfSpeech2.innerText = `${wordArray.meanings[1].partOfSpeech}`;
-    verbMeaning1.innerText= `${wordArray.meanings[1].definitions[0].definition}`;
-    example.innerText = checkUndefined(`${wordArray.meanings[1].definitions[0].example}`)
-    console.log(checkUndefined(`${wordArray.meanings[1].definitions[0].example}`));
-    source.innerText = `${wordArray.sourceUrls}`;
-    source.href = `${wordArray.sourceUrls}`;
-    
+    partOfSpeech.innerText = `${wordArray.meanings[0].partOfSpeech}`;
+    if (wordArray.meanings[0].definitions.length > 1){
+        if (wordArray.meanings[0].definitions.length < 3){
+            definition1.innerText= `${wordArray.meanings[0].definitions[0].definition}`;
+            definition2.innerText= checkUndefined(`${wordArray.meanings[0].definitions[1].definition}`);
+            definition3.remove();
+        }else{
+            definition1.innerText= `${wordArray.meanings[0].definitions[0].definition}`;
+            definition2.innerText= checkUndefined(`${wordArray.meanings[0].definitions[1].definition}`);
+            definition3.innerText= checkUndefined(`${wordArray.meanings[0].definitions[2].definition}`);
+        }      
+    }else{
+        definition1.innerText= `${wordArray.meanings[0].definitions[0].definition}`;
+        definition2.remove();
+        definition3.remove();
+    }
+    const means= wordArray.meanings;
+    const countMean =  wordArray.meanings.length;
+    means.forEach(mean => {
+        if(mean.synonyms.length < 1 ){
+            synonyms.innerText = '';
+           
+        }else{
+            synonyms.innerText = mean.synonyms;
+                      
+        }
 
-    // sourceUrls// adviceTitle.firstChild.innerText = `ADVICE #${advice.id}`;
-    // adviceText.firstChild.innerText = advice.advice;
+        if (countMean>1) {
+            if(mean.partOfSpeech){
+                document.getElementById('verbcontainer').style.display ="block";
+
+                partOfSpeech2.innerText = mean.partOfSpeech;
+                verbMeaning1.innerText= checkUndefined(`${wordArray.meanings[1].definitions[0].definition}`);
+                example.innerText = checkUndefined(`${wordArray.meanings[1].definitions[0].example}`)
+            }else{
+                partOfSpeech2.innerText='';
+                verbMeaning1.innerText='';
+                example.innerText='';
+                SecondMeaning.innerText='';
+                document.getElementById('verbcontainer').style.display ="none";
+            }
+        }else{
+            partOfSpeech2.innerText='';
+            verbMeaning1.innerText='';
+            example.innerText='';
+            SecondMeaning.innerText='';
+            document.getElementById('verbcontainer').style.display ="none";
+        }
+        
+    });
+    source.innerText = checkUndefined(`${wordArray.sourceUrls}`);
+    source.href = checkUndefined(`${wordArray.sourceUrls}`);
+    const audioSource= `${wordArray.phonetics[0].audio}`;
+    console.log(audioSource);
+    // function playSound () {
+    //     let ding = new Audio('ding.mp3');
+    //     ding.play();
+    // }
+
     // For example, the li:nth-child(2) selects the second <li> element in a list:
-
     // let listItem = document.querySelectorAll('li:nth-child(2)');
     // To select all li elements that are directly inside a <ul> element with the class nav:
     // let listItems = document.querySelectorAll('ul.nav > li');
@@ -72,34 +114,14 @@ function displayWord(wordArray){
 async function getapi(url,wordNew){
           //storing response
         const response = await fetch(`${url}/${wordNew}`);
-
-        //storing data in form of JSon
-        var data = await response.json();
-        const element = data[0];
-        displayWord(element)   
-        // return element;
-        
-        // if (response) {
-        //     displayAdvice(element)
-
-       
-    
+        if (response.ok) {
+            //storing data in form of JSon
+            var data = await response.json();
+            const element = data[0];
+            displayWord(element);  
+        }
+         else{
+            alert("word not available");
+         }
     } 
 
-    //  var data = '{"name": "mkyong","age": 30,
-    //  "address": {"streetAddress": "88 8nd Street","city": "New York"},
-    //  "phoneNumber": [{"type": "home","number": "111 111-1111"},
-    //  {"type": "fax","number": "222 222-2222"}]}';
-
-	// var json = JSON.parse(data);
-			
-	// alert(json["name"]); //mkyong
-	// alert(json.name); //mkyong
-	
-	// alert(json.address.streetAddress); //88 8nd Street
-	// alert(json["address"].city); //New York
-			
-	// alert(json.phoneNumber[0].number); //111 111-1111
-	// alert(json.phoneNumber[1].type); //fax
-			
-	// alert(json.phoneNumber.number); //undefined
